3458ae17641120a7610cf472fe9a8379027758df
kolynzb <collinsbenda360@gmail.com>
2023-11-08 02:57:41 +0300

    feat: add logo variations

--- config/site.ts
+++ config/site.ts
@@ -4,8 +4,8 @@ export const siteConfig = {
   name: "Africash",
   description: "Africa In One Financial Platform",
   logo: {
-    dark: "/images/logos/logo.png",
-    light: "/images/logos/logo.png",
+    dark: "/images/logos/dark.png",
+    light: "/images/logos/light.png",
   },
   url: "https://africash.vercel.app/",
   navItems: [
