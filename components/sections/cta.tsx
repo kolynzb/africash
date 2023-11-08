c7109bbad4a373492d7790615d852da10265d94b
kolynzb <collinsbenda360@gmail.com>
2023-11-08 16:06:43 +0300

    style: add squiggly lines

--- components/sections/cta.tsx
+++ components/sections/cta.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import Container from "../layout/container";
 import Image from "next/image";
 import { AnimatedSlide } from "../animations/slide";
+import SquigglyLines from "../squiggly-lines";
 
 const CTA = () => {
   return (
-    <AnimatedSlide className="bg-accent px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-lg md:px-24 lg:px-2 lg:py-4 rounded-md my-10">
+    <AnimatedSlide className="relative bg-accent px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-lg md:px-24 lg:px-2 lg:py-4 rounded-md my-20 overflow-hidden">
+      <SquigglyLines className="absolute inset-0 scale-[2] z-[1] h-full w-full opacity-[12%] rotate-12" />
+      <SquigglyLines className="absolute -right-[601px] scale-[2] z-[1] h-full w-full opacity-[12%] rotate-12" />
+      <SquigglyLines className="absolute -left-[610px] scale-[2] z-[1] h-full w-full opacity-[12%] rotate-12" />
       <div className="text-center w-full mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 z-20">
         <h2 className="text-lg font-extrabold text-white   sm:text-4xl">
-        Do business with the world
+          Do business with the world
         </h2>
 
         <div className="lg:mt-0 lg:flex-shrink-0">
