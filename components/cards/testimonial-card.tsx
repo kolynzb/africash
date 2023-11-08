94ec1cf54e42676c3cfc8e87150dbededc105f62
kolynzb <collinsbenda360@gmail.com>
2023-11-08 07:05:05 +0300

    feat: create testimonials card

---
+++ components/cards/testimonial-card.tsx
@@ -0,0 +1,49 @@
+import { Quote } from "lucide-react";
+import Image from "next/image";
+import React from "react";
+
+type Props = {
+  testimonial: string;
+  name: string;
+  designation: string;
+  avatar: string;
+};
+
+const TestimonialCard = (props: Props) => {
+  return (
+    <div className="mx-auto mt-8 rounded-lg shadow max-w-md px-10 py-2 border border-gray-200 font-light leading-snug flex flex-col justify-between">
+      <div className="mt-2 relative">
+        <div className="-ml-4">
+          <Quote className="w-8 opacity-25 text-accent" />
+        </div>
+        {props.testimonial}
+        <div className="-mr-4 absolute right-4">
+          <Quote className="w-8 opacity-25 text-accent" />
+        </div>
+      </div>
+
+      <div>
+        <div className="mx-auto w-full border  my-8"></div>
+        <div className="flex items-center">
+          <div>
+            <Image
+              className="w-12 h-12 rounded-full "
+              src={props.avatar}
+              width={50}
+              height={50}
+              alt={`${props.name}'s name`}
+            />
+          </div>
+          <div className="ml-4">
+            <div className="font-bold">{props.name}</div>
+            <div className="text-sm text-gray-600 mt-1">
+              {props.designation}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TestimonialCard;
