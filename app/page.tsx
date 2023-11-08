c7c5d95ae68d2893d4a77f0b680759ec80040b9c
kolynzb <collinsbenda360@gmail.com>
2023-11-08 04:43:17 +0300

    feat: add hero section to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import Hero from "@/components/sections/hero";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="mx-auto min-h-screen ">
+      <Hero />
+    </main>
   );
 }
