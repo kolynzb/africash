929fd204124dfba52be4716c74a34505f24e2e07
kolynzb <collinsbenda360@gmail.com>
2023-11-08 05:06:56 +0300

    feat: add brands

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Brands from "@/components/sections/brands";
 import Hero from "@/components/sections/hero";
 
 export default function Home() {
   return (
     <main className="mx-auto min-h-screen ">
       <Hero />
+      <Brands />
     </main>
   );
 }
