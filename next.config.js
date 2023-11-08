3198e713a20a2a9489b2d6a76a359bfe4c3da449
kolynzb <collinsbenda360@gmail.com>
2023-11-08 14:04:39 +0300

    chore: add image remote patterns

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{ protocol: "https", hostname: "res.cloudinary.com" }],
+    remotePatterns: [
+      { protocol: "https", hostname: "res.cloudinary.com" },
+      { protocol: "https", hostname: "randomuser.me" },
+      { protocol: "https", hostname: "robohash.org" },
+    ],
   },
 };
 
