31b29265783933adf6074de66cfbbc20cd695cd4
kolynzb <collinsbenda360@gmail.com>
2023-11-08 05:10:45 +0300

    refactor: make SEO freidnly

--- components/sections/brands.tsx
+++ components/sections/brands.tsx
@@ -8,65 +8,66 @@ const Brands = () => {
   return (
     <AnimatedSlide className="bg-[#EDF0F5] my-4">
       <Container className="mt-12 grid grid-cols-3 sm:grid-cols-4 md:grid-cols-6 ">
-        <div className="p-4 grayscale transition duration-200 hover:grayscale-0">
+        <figure className="p-4 grayscale transition duration-200 hover:grayscale-0">
           <Image
             src="./images/clients/microsoft.svg"
             className="h-12 w-auto mx-auto"
             loading="lazy"
             alt="client logo"
-            fill={true}
+            width={50}
+            height={50}
           />
-        </div>
-        <div className="p-4 grayscale transition duration-200 hover:grayscale-0">
-          <img
+        </figure>
+        <figure className="p-4 grayscale transition duration-200 hover:grayscale-0">
+          <Image
             src="./images/clients/airbnb.svg"
             className="h-12 w-auto mx-auto"
             loading="lazy"
             alt="client logo"
-            width=""
-            height=""
+            width={50}
+            height={50}
           />
-        </div>
-        <div className="p-4 flex grayscale transition duration-200 hover:grayscale-0">
-          <img
+        </figure>
+        <figure className="p-4 flex grayscale transition duration-200 hover:grayscale-0">
+          <Image
             src="./images/clients/google.svg"
             className="h-9 w-auto m-auto"
             loading="lazy"
             alt="client logo"
-            width=""
-            height=""
+            width={50}
+            height={50}
           />
-        </div>
-        <div className="p-4 grayscale transition duration-200 hover:grayscale-0">
-          <img
+        </figure>
+        <figure className="p-4 grayscale transition duration-200 hover:grayscale-0">
+          <Image
             src="./images/clients/ge.svg"
             className="h-12 w-auto mx-auto"
             loading="lazy"
             alt="client logo"
-            width=""
-            height=""
+            width={50}
+            height={50}
           />
-        </div>
-        <div className="p-4 flex grayscale transition duration-200 hover:grayscale-0">
-          <img
+        </figure>
+        <figure className="p-4 flex grayscale transition duration-200 hover:grayscale-0">
+          <Image
             src="./images/clients/netflix.svg"
             className="h-8 w-auto m-auto"
             loading="lazy"
             alt="client logo"
-            width=""
-            height=""
+            width={50}
+            height={50}
           />
-        </div>
-        <div className="p-4 grayscale transition duration-200 hover:grayscale-0">
-          <img
+        </figure>
+        <figure className="p-4 grayscale transition duration-200 hover:grayscale-0">
+          <Image
             src="./images/clients/google-cloud.svg"
             className="h-12 w-auto mx-auto"
             loading="lazy"
             alt="client logo"
-            width=""
-            height=""
+            width={50}
+            height={50}
           />
-        </div>
+        </figure>
       </Container>
     </AnimatedSlide>
   );
